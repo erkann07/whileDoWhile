24dcbcbf531932c85fb0898b9030038a28a9fe58
Erkan <erkankrgz0707@gmail.com>
2025-05-22 23:17:38 +0300

    Update whileDoWhile.cs

--- whileDoWhile.cs
+++ whileDoWhile.cs
@@ -16,3 +16,4 @@ do
     Console.WriteLine("Ben bir Patika'lıyım");
     sayac2++;
 } while (sayac2 < limit2);
+//While da koşul sağlanırsa döngüye giriyor doWthile da ise önce döngü 1 kez çalıştırılıyor sonra koşul sağlanırsa öyle döngüye giriyor.
