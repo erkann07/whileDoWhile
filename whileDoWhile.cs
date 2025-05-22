2732510a9929755ba632e16185f3212fb70221d5
Erkan <erkankrgz0707@gmail.com>
2025-05-22 23:16:02 +0300

    Add files via upload

---
+++ whileDoWhile.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Bir Limit Değeri Giriniz.");
+int limit = int.Parse(Console.ReadLine());
+int sayac = 0;
+while (sayac < limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+}
+
+    Console.WriteLine("Bir Limimt Değeri Giriniz.");
+    int limit2 = Convert.ToInt32(Console.ReadLine());
+    int sayac2 = 0;
+   
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac2++;
+} while (sayac2 < limit2);
